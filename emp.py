6488699d246152b4bb7d1b9185c7726f23d92d07
kuberseth002 <kuber0804.be21@chitkara.edu.in>
2025-02-04 22:14:50 +0530

    [kuber] Add: Checking the employee attendance

--- emp.py
+++ emp.py
@@ -1,1 +1,15 @@
-print("Welcome to employee-wage program")
+# print("Welcome to employee-wage program")
+
+import random
+
+def emp_attd():
+    randnumber = random.random()
+    threshold = 0.5
+    if randnumber < threshold:
+        print("absent")
+        return 0
+    else:
+        print("present")
+        return 1
+emp_attd()
+
