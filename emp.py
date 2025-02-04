7efbd6010bd7e04ccacf04e47d327a6f955077a9
kuberseth002 <kuber0804.be21@chitkara.edu.in>
2025-02-04 22:33:10 +0530

    [kuber] Add: Calculate the daily wages of the employee

--- emp.py
+++ emp.py
@@ -1,5 +1,3 @@
-# print("Welcome to employee-wage program")
-
 import random
 
 def emp_attd():
@@ -11,5 +9,33 @@ def emp_attd():
     else:
         print("present")
         return 1
-emp_attd()
+
+def calculate_wages():
+    worktype = random.randint(0, 2)  
+    part_time_work = 4
+    full_time_work = 8
+    wages_per_hour = 20
+    total_hours = 0
+    total_wages = 0
+
+    absent_or_present = emp_attd()
+
+    if absent_or_present == 1:
+        if worktype == 0:
+            total_hours += 0
+            print("No work today")
+            total_wages = 0
+            print(f"Total wages: {total_wages}")
+        elif worktype == 1:
+            total_hours += part_time_work
+            total_wages = part_time_work * wages_per_hour
+            print(f"Total wages: {total_wages}")
+        elif worktype == 2:
+            total_hours += full_time_work
+            total_wages = full_time_work * wages_per_hour
+            print(f"Total wages: {total_wages}")
+        else:
+            print("Invalid input")
+calculate_wages()
+
 
